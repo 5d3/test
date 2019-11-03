a7c6793213a113460410a46866edeb6a847932db
Josh <joshjliu@sina.com>
2019-11-04 05:02:24 +0800

    add new videos, posters, subtitles; changed scrollbar styles

--- docs/myvideo.js
+++ docs/myvideo.js
@@ -7,21 +7,67 @@ $(document).ready(function(){
     //player.displayCaptions();
     console.log("--"); 
   });
+  
+  
+    //var sub = document.createElement( "div" );
+  
+  
   //var vid = document.getElementById("my-player");
 
   var vid = $("#my-player").get(0); //the jQuery method of get(0) returns the DOM element itself
+  var sub = $("<div>Hello jQuery</div>");
+  sub.addClass("subtitle");
+  $(vid).after(sub);
+  var sub_ul = $("<ul></ul>");
+  if(vid.textTracks[0].cues) {
+    $.each(vid.textTracks[0].cues, (index, cue) => {
+      console.log(cue);
+      console.log(index + " : " + cue.startTime + " : " + cue.endTime + " : " + cue.text);
+      var msg = cue.text + "</li>";
+      $(sub).append("<div>" + cue.text + "</div>");
+    });
+  }
+  
   $(vid).on("timeupdate", () => {   //use the $() to wrap a DOM to a jQuery-wrapped DOM element.
     //player.displayCaptions();
     console.log("--" + vid.currentTime + "/" + vid.duration + " : " + vid.textTracks[0] + " crossOrigin: " + vid.crossOrigin );
     if(vid.currentTime > 15){
       vid.currentTime = 0;
     }
+    if(vid.textTracks[0].activeCues && vid.textTracks[0].activeCues.length > 0) {
+      var sub0 = vid.textTracks[0].activeCues[0];
+      console.log("====" + sub0.text);
+      sub[0].innerText = sub0.text;
+    }
   }); 
   
-  //var sub = document.createElement( "div" );
-  sub = $("<div>Hello jQuery</div>");
-  sub.addClass("subtitle");
+  
+  
 
-  $(vid).after(sub);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });		
