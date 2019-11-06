b114550e788b2464b6e1d5aec3abbc82fd54f5fc
Josh <joshjliu@sina.com>
2019-11-07 00:09:46 +0800

    basic function implemented.

--- docs/myvideo.js
+++ docs/myvideo.js
@@ -4,111 +4,91 @@ if (typeof jQuery != 'undefined') {
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('window.DOM fully loaded and parsed');
+    console.log('2, window.DOM fully loaded and parsed');
 });
 
 window.addEventListener('load', (event) => {
-    console.log('window.load - DOM fully loaded and parsed');
+    console.log('4, window.load - DOM fully loaded and parsed');
     var vid1 = $("#my-player").get(0);
 });
 
 window.onload = () => {
-  console.log("window.onload");
-  var vid1 = $("#my-player").get(0);
+  console.log("5, window.onload");
+
+  var vid = $(".my-player").get(0); //the jQuery method of get(0) returns the DOM element itself
+  var sub = $("<div>Hello jQuery</div>");
+  if(vid.textTracks[0]) {
+    $(vid.textTracks[0]).on("cuechange", (ev) => {
+      console.log(vid);
+      console.log("oooooooooooooncuechange fired. " + (ev.currentTarget.activeCues.length > 0 ? ev.currentTarget.activeCues[0].text : ev.currentTarget.activeCues));
+
+      if(ev.currentTarget.activeCues.length > 0) {
+        //var idx = getCueIndex(vid.textTracks[0].cues, ev.currentTarget.activeCues[0].startTime);
+        var idx;
+        $.each( vid.textTracks[0].cues, (index, cue) => {
+          console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA: " + index + "    " + cue.startTime + "  " + ev.currentTarget.activeCues[0].startTime);
+          if(cue.startTime == ev.currentTarget.activeCues[0].startTime) {
+            idx = index;
+            console.log("-------------------------------- " + idx + " " + index);
+          }
+        })
+        console.log("PPPPPP " + idx);
+        var div = $(".subtitle div").get(idx);
+        $(div).toggleClass("currentsub");
+      } else {  //the length == 0, it's the correct time to clear ".currentsub" class
+        $(".currentsub").toggleClass("currentsub");
+      }
+    });
+  
+    function getCueIndex(cues, startTime) {
+      console.log("BBBBBBBBBBBBBBB: " + cues.length);
+      $.each( cues, (index, cue) => {
+        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA: " + index + "    " + cue.startTime + "  " + startTime);
+        if(cue.startTime == startTime) {
+          var idx = index;
+          console.log("-------------------------------- " + idx + " " + index);
+          return idx;
+        }
+      })
+      
+    }
+
+
+    sub.addClass("subtitle");
+    $(vid).after(sub);
+
+    if(vid.textTracks[0].cues) {
+      $.each(vid.textTracks[0].cues, (index, cue) => {
+        console.log(cue);
+        console.log(index + " : " + cue.startTime + " : " + cue.endTime + " : " + cue.text);
+
+        $(sub).append("<div starttime=" + cue.startTime + " endTime=" + cue.endTime + ">" + cue.text + "</div>");
+      });
+    }
+      
+    $(vid).on("timeupdate", () => {   //use the $() to wrap a DOM to a jQuery-wrapped DOM element.
+//-------      console.log("--" + vid.currentTime + "/" + vid.duration + " : " + vid.textTracks[0] + " crossOrigin: " + vid.crossOrigin );$
+      //if(vid.currentTime > 15){
+      //  vid.currentTime = 0;
+      //}
+      if(vid.textTracks[0].activeCues && vid.textTracks[0].activeCues.length > 0) {
+        var sub0 = vid.textTracks[0].activeCues[0];
+//-------        console.log("====" + sub0.text);
+        //sub[0].innerText = sub0.text;
+      }
+    });
+  }
 }
 
+
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('document.DOM fully loaded and parsed');
+    console.log(1, 'document.DOM fully loaded and parsed');
 });
 
 $(document).ready(function(){
-  $("#my-player").on("timeupdate", () => {
-    //player.displayCaptions();
-    console.log("--"); 
-  });
-  
-  
-    //var sub = document.createElement( "div" );
-  
-  
-  //var vid = document.getElementById("my-player");
-
-  var vid = $("#my-player").get(0); //the jQuery method of get(0) returns the DOM element itself
-  var sub = $("<div>Hello jQuery</div>");
-  sub.addClass("subtitle");
-  $(vid).after(sub);
-
-  if(vid.textTracks[0]) {
-    $(vid.textTracks[0]).on("cuechange", () => {
-      console.log("oncuechange fired.");
-    });
-    
-    $(vid.textTracks).on("change", (e) => {
-      console.log("onchange fired.");
-    });
-    $(vid.textTracks).on("addtrack", (e) => {
-      console.log("onaddtrack fired.");
-    });
-    
-    
-    
-    
-    $(vid).on("addtrack", (e) => {
-      console.log("onaddtrack fired.");
-    });
-    
-    vid.textTracks.addEventListener("addtrack", function(e) {console.log("addtrack fired.")}, false);
-    
-    
-    vid.textTracks[0].onchange = () => {
-
-      if(vid.textTracks[0].cues) {
-        $.each(vid.textTracks[0].cues, (index, cue) => {
-          console.log(cue);
-          console.log(index + " : " + cue.startTime + " : " + cue.endTime + " : " + cue.text);
-          var msg = cue.text + "</li>";
-          $(sub).append("<div>" + cue.text + "</div>");
-        });
-      }
-    }
-  }
-  
-  //--------------------delete
-  $(vid).on("addtrack", () => {
-    console.log("addTrack fired!!!!!!!!!!!!");
-  });
-  
-  $(vid).on("timeupdate", () => {   //use the $() to wrap a DOM to a jQuery-wrapped DOM element.
-    //player.displayCaptions();
-    console.log("--" + vid.currentTime + "/" + vid.duration + " : " + vid.textTracks[0] + " crossOrigin: " + vid.crossOrigin );$
-    if(vid.currentTime > 15){
-      vid.currentTime = 0;
-    }
-    if(vid.textTracks[0].activeCues && vid.textTracks[0].activeCues.length > 0) {
-      var sub0 = vid.textTracks[0].activeCues[0];
-      console.log("====" + sub0.text);
-      sub[0].innerText = sub0.text;
-    }
-  }); 
-  
-  
-  
-
-
-const videoElement = document.querySelector('video');
-
-videoElement.textTracks.addEventListener('addtrack', (event) => {
-  console.log(`Video track: ${event.track.label} added`);
-});
-
-
-
-
-
-
-
-
-
+  console.log(3, 'document.ready fully loaded and parsed');
 
 
 
